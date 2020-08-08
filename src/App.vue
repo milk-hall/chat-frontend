83f2e0487f0f56503c8b11496ef8fe2cc9796013
xg <gkinxin@gmail.com>
2020-08-08 18:39:00 +0800

    feat: 添加头部 和 左侧抽屉

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,56 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <chat-header v-bind:showDrawer="showDrawer" />
+    <v-sheet
+      id="scrolling-techniques-2"
+      class="overflow-y-auto"
+      max-height="600px"
+    >
+      <v-container style="height: 100vh"></v-container>
+    </v-sheet>
+    <v-navigation-drawer v-model="drawer" absolute temporary height="100vh">
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="red--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-      <router-view />
-    </v-main>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import ChatHeader from "./components/ChatHeader";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    ChatHeader
   },
 
   data: () => ({
-    //
-  })
+    drawer: false,
+    group: ['/home','/about']
+  }),
+  methods:{
+    showDrawer:function(){
+      this.drawer=true
+    }
+  }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
   {
     path: "/about",
     name: "About",
@@ -22,7 +16,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes
 });
